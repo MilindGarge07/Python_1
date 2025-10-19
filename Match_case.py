53aff3034938ba56c0044a50ae8bc585d66b3ad4
MilindGarge07 <milindgarge07@gmail.com>
2025-10-19 20:45:26 +0530

    done

---
+++ Match_case.py
@@ -0,0 +1,12 @@
+
+var = int(input("Enter a value: "))
+match var:
+    case 20:
+        print("Value is 20")
+    case 30:
+        print("Value is 30")    
+    case _:
+        print("Value is neither 20 nor 30")
+        
+        
+        
---
+++ loop_for.py
@@ -0,0 +1,12 @@
+# fruits = ["apple", "banana", "cherry"]
+# for fru in fruits:
+#     print(fru)
+#     for fru_letter in fru:
+#         print(fru_letter)
+
+counter = 0
+for i in range(5,51,5):
+    counter += 1
+    print("5 *",counter,"=",i)
+    
+        
